c977d928d92077266cdbf64b9e4cb5998820226d
William <whamilton2151@stu.d214.org>
2021-04-23 09:44:56 -0500

    Worked on MVP

--- AssignmentNotebook/ViewController.swift
+++ AssignmentNotebook/ViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UITableViewDataSource {
+  
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-// works 
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    
+    }
+    
 
 }
-
